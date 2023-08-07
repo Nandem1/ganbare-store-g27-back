d0030fbd8fdec811362574552951b62050ca43cb
Katia Velasquez <katia.velasquez@.com>
2023-08-07 01:58:20 -0400

    Se agrega endpoint para decodificacion de token

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,6 +6,7 @@ const authMiddleware = require('../middlewares/authMiddleware')
 router.get('/',userController.getUsers);
 router.post('/register', authMiddleware.checkDuplicateUsername,userController.registerUser);
 router.post('/login', userController.loginUser);
+router.post('/decodeToken', userController.decodeToken);
 router.get('/protected', authMiddleware.authMiddleware, (req, res) => {
   res.json({ message: 'Ruta protegida, acceso permitido.' });
 });
